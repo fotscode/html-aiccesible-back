634b93194174507735698188c1a7f7b1416f7bf6
fots <facundotomatis@gmail.com>
2024-07-25 00:30:17 -0300

    feat(user): added routes to controllers, along with usage of middleware previously added

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -2,6 +2,8 @@ package routes
 
 import (
 	"html-aiccesible/controllers"
+	"html-aiccesible/middleware"
+	"html-aiccesible/models"
 
 	"github.com/gin-gonic/gin"
 	v "github.com/go-playground/validator/v10"
@@ -11,6 +13,17 @@ func SetUpRouter() *gin.Engine {
 	r := gin.Default()
 	validate := v.New()
 	c := controllers.NewController(validate)
-	r.GET("/", c.Ping)
+	api := r.Group("/api", middleware.Errors(), middleware.JSONMiddleware())
+	{
+		api.GET("/", c.Ping)
+		user := api.Group("/user")
+		{
+			user.POST("/add", gin.Bind(models.CreateUserBody{}), c.CreateUser)
+			user.PUT("/update", gin.Bind(models.UpdateUserBody{}), c.UpdateUser)
+			user.GET("/get/:id", c.GetUser)
+			user.DELETE("/delete/:id", c.DeleteUser)
+			user.GET("/list", c.ListUsers)
+		}
+	}
 	return r
 }
