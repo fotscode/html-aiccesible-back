27d13d41af0df8836654a5a254772820bbad67f0
fots <facundotomatis@gmail.com>
2024-07-25 00:27:13 -0300

    feat(utils): added more http response custom functions

--- src/httputil/response.go
+++ src/httputil/response.go
@@ -6,14 +6,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type HTTPOKResponse[T any] struct {
+type HTTPResponse[T any] struct {
 	Code   int    `json:"code"`
 	Status string `json:"status"`
 	Data   T      `json:"data"`
 }
 
-func newHTTPOKResponse[T any](code int, data T) HTTPOKResponse[T] {
-	return HTTPOKResponse[T]{
+func newHTTPResponse[T any](code int, data T) HTTPResponse[T] {
+	return HTTPResponse[T]{
 		Code:   code,
 		Status: http.StatusText(code),
 		Data:   data,
@@ -22,9 +22,21 @@ func newHTTPOKResponse[T any](code int, data T) HTTPOKResponse[T] {
 
 // send using gin
 func OK[T any](c *gin.Context, data T) {
-	c.JSON(http.StatusOK, newHTTPOKResponse(http.StatusOK, data))
+	c.JSON(http.StatusOK, newHTTPResponse(http.StatusOK, data))
 }
 
 func Created[T any](c *gin.Context, data T) {
-	c.JSON(http.StatusCreated, newHTTPOKResponse(http.StatusOK, data))
+	c.JSON(http.StatusCreated, newHTTPResponse(http.StatusOK, data))
+}
+
+func BadRequest[T any](c *gin.Context, data T) {
+	c.AbortWithStatusJSON(http.StatusBadRequest, newHTTPResponse(http.StatusBadRequest, data))
+}
+
+func InternalServerError[T any](c *gin.Context, data T) {
+	c.AbortWithStatusJSON(http.StatusInternalServerError, newHTTPResponse(http.StatusInternalServerError, data))
+}
+
+func NoContent[T any](c *gin.Context, data T) {
+	c.AbortWithStatusJSON(http.StatusNoContent, newHTTPResponse(http.StatusNoContent, data))
 }
