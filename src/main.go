b7dc9b723d11d578768dbd61ce9f017ab00e9a08
fots <facundotomatis@gmail.com>
2024-07-26 00:10:29 -0300

    feat(auth): added create default user to main

--- src/main.go
+++ src/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"html-aiccesible/middleware"
+	"html-aiccesible/models"
 	routes "html-aiccesible/routes"
 )
 
 func main() {
+	models.CreateDefaultUser()
 	r := routes.SetUpRouter()
 	r.Use(middleware.SetupCors())
 	r.Run()
