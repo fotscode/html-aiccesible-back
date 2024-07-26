de1dd8a795c4c8f9925ded48129ee600a301a4c0
fots <facundotomatis@gmail.com>
2024-07-26 00:11:04 -0300

    feat(auth): added routes along with middleware, also removed unused validator from controller

--- src/controllers/controller.go
+++ src/controllers/controller.go
@@ -1,15 +1,8 @@
 package controllers
 
-import (
-	v "github.com/go-playground/validator/v10"
-)
-
 type Controller struct {
-	Validator *v.Validate
 }
 
-func NewController(validator *v.Validate) *Controller {
-	return &Controller{
-		Validator: validator,
-	}
+func NewController() *Controller {
+	return &Controller{}
 }
