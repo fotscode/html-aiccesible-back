725f37b12f58d718ca56cd276ec1450562d5f743
fots <facundotomatis@gmail.com>
2024-07-28 21:08:22 -0300

    feat(posts): added comment create/update body models

--- src/models/comment.go
+++ src/models/comment.go
@@ -9,3 +9,15 @@ type Comment struct {
 	PostID  uint   `json:"post_id"`
 	UserID  uint   `json:"user_id"`
 }
+
+type CreateCommentBody struct {
+	Title   string `json:"title" binding:"required,min=5,max=100"`
+	Content string `json:"content" binding:"required,min=5,max=1000"`
+	PostID  uint   `json:"post_id" binding:"required"`
+}
+
+type UpdateCommentBody struct {
+	ID      uint   `json:"id" binding:"required"`
+	Title   string `json:"title" binding:"required,min=5,max=100"`
+	Content string `json:"content" binding:"required,min=5,max=1000"`
+}
