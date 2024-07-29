6df7099817fbfeee54b3924313b7bf7749ca0b1f
fots <facundotomatis@gmail.com>
2024-07-28 21:10:34 -0300

    feat(posts): added comment controller functions

---
+++ src/controllers/comment.go
@@ -0,0 +1,62 @@
+package controllers
+
+import (
+	"html-aiccesible/httputil"
+	m "html-aiccesible/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (b *Controller) CreateComment(c *gin.Context) {
+	body := c.MustGet(gin.BindKey).(*m.CreateCommentBody)
+	user := c.MustGet("user").(*m.User)
+	comment, err := b.CommentRepo.CreateComment(user, body)
+	if err != nil {
+		httputil.InternalServerError(c, err)
+		return
+	}
+	httputil.OK(c, comment)
+}
+
+func (b *Controller) UpdateComment(c *gin.Context) {
+	body := c.MustGet(gin.BindKey).(*m.UpdateCommentBody)
+	user := c.MustGet("user").(*m.User)
+	comment, err := b.CommentRepo.UpdateComment(user, body)
+	if err != nil {
+		httputil.InternalServerError(c, err)
+		return
+	}
+	httputil.OK(c, comment)
+}
+
+func (b *Controller) ListComments(c *gin.Context) {
+	lo := c.MustGet("lo").(*m.ListOptions)
+	getOpt := c.MustGet("getOpt").(*m.GetOptions)
+	comments, err := b.CommentRepo.ListComments(lo.Page, lo.Size, getOpt.Id)
+	if err != nil {
+		httputil.InternalServerError(c, err)
+		return
+	}
+	httputil.OK(c, comments)
+}
+
+func (b *Controller) GetComment(c *gin.Context) {
+	getOpt := c.MustGet("getOpt").(*m.GetOptions)
+	comment, err := b.CommentRepo.GetComment(getOpt.Id)
+	if err != nil {
+		httputil.NotFound(c, err)
+		return
+	}
+	httputil.OK(c, comment)
+}
+
+func (b *Controller) DeleteComment(c *gin.Context) {
+	getOpt := c.MustGet("getOpt").(*m.GetOptions)
+	user := c.MustGet("user").(*m.User)
+	err := b.CommentRepo.DeleteComment(user, getOpt.Id)
+	if err != nil {
+		httputil.NotFound(c, err)
+		return
+	}
+	httputil.NoContent(c, "Deleted comment successfully")
+}
