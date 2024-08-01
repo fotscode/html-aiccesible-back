eef6333823277d4cbec0c358d923a00527908cbe
fots <facundotomatis@gmail.com>
2024-07-31 22:24:02 -0300

    feat(comments): added get comment test, added create comment helper

--- src/test/comment_test.go
+++ src/test/comment_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"fmt"
 	"html-aiccesible/httputil"
 	"html-aiccesible/models"
 	"html-aiccesible/routes"
@@ -10,11 +11,47 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func createComment(t *testing.T, r *gin.Engine, postID uint, token string) *models.Comment {
+	var res httputil.HTTPResponse[*models.Comment]
+	w := createRequest(t, r, http.MethodPost, "/api/comment/add", models.CreateCommentBody{
+		PostID:  postID,
+		Title:   generateRandomString(10),
+		Content: generateRandomString(10),
+	}, &res, token)
+	if w.Code != http.StatusCreated {
+		t.Errorf("Expected %d but got %d", http.StatusCreated, res.Code)
+	}
+	return res.Data
+}
+
 func TestGetComment(t *testing.T) {
 	gin.SetMode(gin.TestMode)
 	r := routes.SetUpRouter()
 
-	tests := []TestBody[string]{}
+	_, token := login(t, r, false)
+	post := createPost(t, r, token)
+	comment := createComment(t, r, post.ID, token)
+
+	tests := []TestBody[string]{
+		{
+			Name:         "Get comment by id successfully",
+			Path:         fmt.Sprintf("/%d", comment.ID),
+			ExpectedCode: http.StatusOK,
+			RespContains: comment.Title,
+		},
+		{
+			Name:         "Get comment by id not found",
+			Path:         "/262144",
+			ExpectedCode: http.StatusNotFound,
+			RespContains: "",
+		},
+		{
+			Name:         "Get comment by id invalid id",
+			Path:         "/invalid",
+			ExpectedCode: http.StatusBadRequest,
+			RespContains: "Invalid ID",
+		},
+	}
 
 	for _, test := range tests {
 		t.Run(test.Name, func(t *testing.T) {
