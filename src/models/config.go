49ea64f99a15d1b3bdf16e8f8da7957c1f20aa42
facundom <facundomiglierini@gmail.com>
2024-08-30 15:28:13 -0300

    Changed data types on config parameters

--- src/models/config.go
+++ src/models/config.go
@@ -7,9 +7,9 @@ type Configuration struct {
 	ShowLikes    bool   `json:"show_likes" gorm:"default:true"`
 	ShowComments bool   `json:"show_comments" gorm:"default:true"`
 	Theme        string `json:"theme" gorm:"default:'light'"`
-	Language     string `json:"language" gorm:"default:'en'"`
-	SizeTitle    int    `json:"size_title" gorm:"default:20"`
-	SizeText     int    `json:"size_text" gorm:"default:14"`
+	Language     string `json:"language" gorm:"default:'es'"`
+	SizeTitle    float32`json:"size_title" gorm:"default:1.0"`
+	SizeText     float32`json:"size_text" gorm:"default:1.0"`
 	UserID       uint   `json:"-"`
 }
 
@@ -18,16 +18,16 @@ type UpdateConfigBody struct {
 	ShowComments bool   `json:"show_comments"`
 	Theme        string `json:"theme"`
 	Language     string `json:"language"`
-	SizeTitle    int    `json:"size_title"`
-	SizeText     int    `json:"size_text"`
+	SizeTitle    float32`json:"size_title"`
+	SizeText     float32`json:"size_text"`
 }
 
 func FillConfigDefaults(config *Configuration) Configuration {
 	config.ShowLikes = true
 	config.ShowComments = true  
 	config.Theme = "light"
-	config.Language = "en"
-	config.SizeTitle = 20
-	config.SizeText = 14
+	config.Language = "es"
+	config.SizeTitle = 1.0
+	config.SizeText = 1.0
 	return *config
 }
